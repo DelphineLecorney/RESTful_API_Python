dac99d7f0ac2d3b229e7a2cc5d5f0cb78d532c33
DelphineLecorney <lecorney.delphine@gmail.com>
2023-07-24 14:43:42 +0200

    Add file connection db

---
+++ config/database.py
@@ -0,0 +1,24 @@
+import pymysql
+
+# Function for connect to the database
+def connectDatabase():
+    try:
+        connection = pymysql.connect(
+            host='localhost',
+            user= 'root',
+            password= '',
+            database= 'api'
+        )
+        if connection.open:
+            print('Successful connection to the database')
+            return connection
+        
+    except pymysql.Error as error:
+        print('Error connecting to the database', error)
+    return None
+
+def closeConnection(connection):
+    if connection.open:
+        connection.close()
+        print('Connection to database closed')
+
