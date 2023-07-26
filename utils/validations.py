11c6eff79edc7c869cb6b67d70ca2a10b3593c1d
DelphineLecorney <lecorney.delphine@gmail.com>
2023-07-26 15:33:13 +0200

    Add function isValidTitle

--- utils/validations.py
+++ utils/validations.py
@@ -1,4 +1,10 @@
 # Function for check if there's a string or not
+
+# strip() : The method removes any leading, and trailing whitespaces.
+
 def isNonEmptyString(s):
     return isinstance(s, str) and len(s.strip) > 0
+
+def isValidTitle(title):
+    return isinstance(title, str) and len(title.strip()) > 0
 
