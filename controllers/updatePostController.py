d07a890cd5fc6fd241f46e030a835801decf0296
DelphineLecorney <lecorney.delphine@gmail.com>
2023-07-26 14:16:33 +0200

    Add new file updatePostController

---
+++ controllers/updatePostController.py
@@ -0,0 +1,25 @@
+# Import modules
+from flask import jsonify, request
+from models.Post import Post
+from config.config import app, db
+
+# Define a route to retrieve all items
+@app.route('/api/v1/posts', methods=['PUT'])
+
+#Function for update a post
+def updatePOst(post_id):
+    post = Post.query.get(post_id)
+    if not post:
+        return jsonify({"error": "Post not found"}), 404
+    
+    data = request.get_json()
+    post.title = data.get('title', post.title)
+    post.body = data.get('body', post.body)
+    post.author = data.get('author', post.author)
+
+    try:
+        db.session.commit()
+        return jsonify({'message', 'The post was successfully updated'}), 200
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({'error':'Error updating post', 'details': str(e)}), 500
